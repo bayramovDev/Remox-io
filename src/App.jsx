6ce36a7cf5d0361290763d1db18b392e1f367565
Bayramzade066 <bayramzade.313@gmail.com>
2022-08-18 02:29:23 +0400

    new website design

--- src/App.jsx
+++ src/App.jsx
@@ -5,19 +5,25 @@ import Navbar from "./sections/navbar";
 import Sponsor from "./sections/sponsor";
 import Main from "./sections/main";
 import Dinamic from "./sections/dinamic";
+import TextAnimation from "./sections/textAnimation";
+import ScrollToTop from "react-scroll-up";
 
 function App() {
   return (
     <div >
-      <div className="bg-[url('../public/header_background.png')] bg-no-repeat bg-cover bg-right bg-local bg-start">
-      <Navbar />
-      <Header />
-      <Main/>
+      <ScrollToTop showUnder={1500} easing={'linear'} duration={800} style={{zIndex: 9999,right: 50, bottom: 50}}>
+        <div className="absolute z-[9999] rounded-full hidden sm:flex items-center justify-center p-1 min-w-[24.5px] sm:min-w-[45px] bg-black hover:bg-[#ff5413] hover:text-black transition-all hover:transitin-all text-primary text-xl sm:text-3xl">&#8682;</div>
+      </ScrollToTop>
+      <div className="bg-[url('../public/header_background.png')] bg-local bg-cover sm:bg-auto 2xl:bg-cover bg-right-top bg-no-repeat ">
+        <Navbar />
+        <Header />
+        <Main />
       </div>
-      <Dinamic/>
+      <TextAnimation />
+      <Dinamic />
       <Sponsor />
-      <Info />
-      <Footer/>
+      {/* <Info /> */}
+      <Footer />
     </div>
   );
 }
